ab1aba6537e3ec5e00d3292311bc92bc6f212233
Alberto de Gracia Recas <alber_recas@hotmail.com>
2018-10-02 19:55:59 +0200

    Initial commit

---
+++ include/offbPID.h
@@ -0,0 +1,29 @@
+#ifndef OFFBPID_H
+#define OFFBPID_H
+
+#include <ros/ros.h>
+#include <stdlib.h>
+#include "geometry_msgs/Pose.h"
+
+using namespace geometry_msgs;
+using namespace std;
+
+namespace offbPID {
+
+	class OffbPID
+	{
+  public:
+  OffbPID();
+
+  private:
+
+  ros::NodeHandle m_nh;
+
+  Pose m_startPose;
+  Pose m_actualPose;
+  Pose m_finalPose;
+
+
+  };
+}
+#endif
