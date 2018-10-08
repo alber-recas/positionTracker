8467fc65acde92d4d726875436e6cf4ed2fb3729
Alberto de Gracia Recas <alber_recas@hotmail.com>
2018-10-08 17:26:51 +0200

    RoverPosSim compiling

--- include/position_tracker/roverPosSim.h
+++ include/position_tracker/roverPosSim.h
@@ -1,42 +1,47 @@
-#define ROVERPOSSIM_H
-#ifndef ROVERPOSSIM_H
+#ifndef KOBUKICONTROLLER_H
+#define KOBUKICONTROLLER_H
 
 #include <ros/ros.h>
 #include <stdlib.h>
 #include "geometry_msgs/Pose.h"
+#include "geometry_msgs/PointStamped.h"
+#include "geometry_msgs/Twist.h"
+#include "geometry_msgs/Vector3.h"
+#include "nav_msgs/GetPlan.h"
 #include "std_msgs/Float32.h"
 
+
 using namespace geometry_msgs;
+using namespace nav_msgs;
+using namespace std_msgs;
+
 using namespace std;
 
-namespace rover_position_simulator{
+namespace rover_position_simulator {
 
 	class RoverPosSim
 	{
   public:
-  	RoverPosSim();
+      RoverPosSim();
+
+			void updateVel(const Float32::ConstPtr& vel);
+			void updateInitPos(const Pose::ConstPtr& initPose);
+			void updateGoalPos(const Pose::ConstPtr& goalPose);
+			void updateActualPos();
+
+			bool m_goalReached; //TODO review public members
+			Pose m_init_pos;  	//TODO review public members
+			Pose m_actual_pos;  //TODO review public members
+			Pose m_goal_pos;  	//TODO review public members
+			float m_vel;  			//TODO review public members
+			int m_sampleRate;  	//TODO review public members
 
   private:
-		ros::NodeHandle m_nh;
+    ros::NodeHandle m_nh;
 
 		ros::Publisher m_pos_pub;
 		ros::Subscriber m_vel_sub, m_init_sub, m_goal_sub;
 
-	  Pose m_init_pos;
-	  Pose m_actual_pos;
-	  Pose m_goal_pos;
-		float m_vel;
-		bool m_goalReached;
-
-		int m_sampleRate;
-
-		void updateVel(const Float32::ConstPtr& vel);
-		void updateInitPos(const Pose::ConstPtr& initPose);
-		void updateGoalPos(const Pose::ConstPtr& goalPose);
-
-		void updateActualPos();
-
-
-  };
+	};
 }
-#endif
+#endif // RRTSTAR_H
--- src/roverPosSim.cpp
+++ src/roverPosSim.cpp
@@ -1,95 +1,89 @@
 #include "position_tracker/roverPosSim.h"
-//#include <geometry_msgs/PoseStamped.h>
-//#include <mavros_msgs/CommandBool.h>
-//#include <mavros_msgs/SetMode.h>
-//#include <mavros_msgs/State.h>
-//#include "math.h"
 
-namespace rover_position_simulator{
+namespace rover_position_simulator {
 
-RoverPosSim::RoverPosSim():m_nh("~"){
+	RoverPosSim::RoverPosSim():m_nh("~"){
 
-//  poseUpdater_sub = nh_.subscribe("/pose", 1, &KobukiController::poseUpdaterCallback, this);
-	m_pos_pub = m_nh.advertise<geometry_msgs::Pose>( "/actual_pos", 10 );
-	m_vel_sub = m_nh.subscribe("/vel", 1, &RoverPosSim::updateVel, this);
-	m_init_sub = m_nh.subscribe("/init_pos", 1, &RoverPosSim::updateInitPos, this);
-	m_goal_sub = m_nh.subscribe("/goal_pos", 1, &RoverPosSim::updateGoalPos, this);
-
-}
-
-RoverPosSim::updateVel(const Float32::ConstPtr& vel){
-	m_vel = vel->data;
-}
-
-RoverPosSim::updateInitPos(const Pose::ConstPtr& initPose){
-	m_init_pos = *initPose;
-}
-
-RoverPosSim::updateGoalPos(const Pose::ConstPtr& goalPose){
-	m_goal_pos = *goalPose;
-}
-
-RoverPosSim::updateActualPos(){
-
-	Pose actualPose = m_actual_pos;
-	Pose new_pos;
-	float step = vel/m_sampleRate;
-
-	if(m_actual_pos.position.y == m_goal_pos.position.y){
-		m_goalReached = true;
+		m_pos_pub = m_nh.advertise<geometry_msgs::Pose>( "/actual_pos", 10 );
+		m_vel_sub = m_nh.subscribe("/vel", 1, &RoverPosSim::updateVel, this);
+		m_init_sub = m_nh.subscribe("/init_pos", 1, &RoverPosSim::updateInitPos, this);
+		m_goal_sub = m_nh.subscribe("/goal_pos", 1, &RoverPosSim::updateGoalPos, this);
 	}
 
-	if(m_actual_pos.position.y == m_init_pos.position.y){
-		m_goalReached = false;
+	void RoverPosSim::updateVel(const Float32::ConstPtr& vel){
+		m_vel = vel->data;
 	}
 
-	if(!m_goalReached){
-		//new_pos.position.x = m_actual_pos.position.x + step;
-		new_pos.position.y = m_actual_pos.position.y + step;
-	}else{
-		new_pos.position.y = m_actual_pos.position.y - step;
+	void RoverPosSim::updateInitPos(const Pose::ConstPtr& initPose){
+		m_init_pos = *initPose;
 	}
-	// Pose distance;
-	// distance.position.x = abs(m_init_pos.position.x - m_goal_pos.position.x);
-	// distance.position.y = abs(m_init_pos.position.y - m_goal_pos.position.y);
-	// distance.position.z = abs(m_init_pos.position.z - m_goal_pos.position.z);
 
-	m_pos_pub.publish(new_pos);
-	m_actual_pos = new_pos;
-}
+	void RoverPosSim::updateGoalPos(const Pose::ConstPtr& goalPose){
+		m_goal_pos = *goalPose;
+	}
+
+	void RoverPosSim::updateActualPos(){
+
+		Pose actualPose = m_actual_pos;
+		Pose new_pos;
+		float step = m_vel/m_sampleRate;
+
+		if(m_actual_pos.position.y == m_goal_pos.position.y){
+			m_goalReached = true;
+		}
+
+		if(m_actual_pos.position.y == m_init_pos.position.y){
+			m_goalReached = false;
+		}
+
+		if(!m_goalReached){
+			//new_pos.position.x = m_actual_pos.position.x + step;
+			new_pos.position.y = m_actual_pos.position.y + step;
+		}else{
+			new_pos.position.y = m_actual_pos.position.y - step;
+		}
+		// Pose distance;
+		// distance.position.x = abs(m_init_pos.position.x - m_goal_pos.position.x);
+		// distance.position.y = abs(m_init_pos.position.y - m_goal_pos.position.y);
+		// distance.position.z = abs(m_init_pos.position.z - m_goal_pos.position.z);
+
+		m_pos_pub.publish(new_pos);
+		m_actual_pos = new_pos;
+	}
 
 }
 
 using namespace rover_position_simulator;
 int main(int argc, char **argv)
 {
-		ROS_INFO("Entering node");
-    ros::init(argc, argv, "position_tracker_node");
+	//ROS_INFO("Entering node");
+	ros::init(argc, argv, "position_tracker_node");
 
-    ros::NodeHandle nh;
+	ros::NodeHandle nh;
 
-		RoverPosSim roverPosSim;
-		m_sampleRate=20;
-		ros::Rate loop_rate(m_sampleRate);
+	RoverPosSim roverPosSim;
+	roverPosSim.m_sampleRate=20;
+	ros::Rate loop_rate(roverPosSim.m_sampleRate);
 
-		//Default initial values for position and velocity
-		m_vel = 1.0; // m/s
-		m_init_pos.position.x = 5;
-		m_init_pos.position.y = 0;
-		m_init_pos.position.z = 0;
+	//Default initial values for position and velocity
+	roverPosSim.m_vel = 1.0; // m/s
+	roverPosSim.m_init_pos.position.x = 5;
+	roverPosSim.m_init_pos.position.y = 0;
+	roverPosSim.m_init_pos.position.z = 0;
 
-		m_goal_pos.position.x = 5;
-		m_goal_pos.position.y = 5;
-		m_goal_pos.position.z = 0;
+	roverPosSim.m_goal_pos.position.x = 5;
+	roverPosSim.m_goal_pos.position.y = 5;
+	roverPosSim.m_goal_pos.position.z = 0;
 
-		m_goalReached=false;
-		m_actual_pos = m_init_pos;
+	roverPosSim.m_goalReached=false;
+	roverPosSim.m_actual_pos = roverPosSim.m_init_pos;
 
-    while (ros::ok()){
-			//roverPosSim.updateActualPos();
-  		ros::spinOnce();
-  		loop_rate.sleep();
-  	}
+	while (ros::ok()){
+		//roverPosSim.updateActualPos();
+		ros::spinOnce();
+		loop_rate.sleep();
+	}
 
-    return 0;
+	return 0;
+
 }
